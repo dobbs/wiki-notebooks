9929222deb0cd88b8cd90979db5265290cd1f93b
Eric Dobbs <eric@dobbse.net>
2025-09-23 13:59:12 -0600

    rebuild

---
+++ docs/assets/cube-CQuYjKws.js
@@ -0,0 +1,246 @@
+import{d}from"./index-Cr1pUUcW.js";d({root:document.getElementById("cell-2"),expanded:[],variables:[]},{id:2,body:(r,e,t,a,l,s,n,o,i,$,c,f,k)=>r`<svg viewBox="-200 -200 400 400" height="500px" transform="rotate(-90)">
+  <g data-axis="x" transform="rotate(0) translate(${e})" stroke="black" stroke-width="1" fill="none">
+    <circle data-rail="outside" r="${t+a}" />
+    <circle data-rail="center"  r="${t}" />
+    <circle data-rail="inside"  r="${t-a}" />
+  </g>
+  <g data-axis="y" transform="rotate(120) translate(${e})" stroke="black" stroke-width="1" fill="none">
+    <circle data-rail="outside" r="${t+a}" />
+    <circle data-rail="center"  r="${t}" />
+    <circle data-rail="inside"  r="${t-a}" />
+  </g>
+  <g data-axis="z" transform="rotate(-120) translate(${e})" stroke="black" stroke-width="1" fill="none">
+    <circle data-rail="outside" r="${t+a}" />
+    <circle data-rail="center"  r="${t}" />
+    <circle data-rail="inside"  r="${t-a}" />
+  </g>
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+l}) translate(${t+a})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+s}) translate(${t+a})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+n}) translate(${t+a})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+o}) translate(${t})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+i}) translate(${t})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+$}) translate(${t})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+c}) translate(${t-a})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+f}) translate(${t-a})"
+  />
+
+  <circle fill="red" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${150+k}) translate(${t-a})"
+  />
+
+
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+l}) translate(${t+a})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+s}) translate(${t+a})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+n}) translate(${t+a})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+o}) translate(${t})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+i}) translate(${t})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+$}) translate(${t})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+c}) translate(${t-a})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+f}) translate(${t-a})"
+  />
+
+  <circle fill="white" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${150+k}) translate(${t-a})"
+  />
+
+
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+l}) translate(${t+a})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+s}) translate(${t+a})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+n}) translate(${t+a})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+o}) translate(${t})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+i}) translate(${t})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+$}) translate(${t})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+c}) translate(${t-a})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+f}) translate(${t-a})"
+  />
+
+  <circle fill="blue" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${150+k}) translate(${t-a})"
+  />
+
+
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+c}) translate(${t-a})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+f}) translate(${t-a})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+k}) translate(${t-a})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+o}) translate(${t})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+i}) translate(${t})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+$}) translate(${t})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+l}) translate(${t+a})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+s}) translate(${t+a})"
+  />
+
+  <circle fill="yellow" stroke="black" r="5"
+          transform="rotate(0) translate(${e}) rotate(${210+n}) translate(${t+a})"
+  />
+
+
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+c}) translate(${t-a})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+f}) translate(${t-a})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+k}) translate(${t-a})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+o}) translate(${t})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+i}) translate(${t})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+$}) translate(${t})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+l}) translate(${t+a})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+s}) translate(${t+a})"
+  />
+
+  <circle fill="green" stroke="black" r="5"
+          transform="rotate(120) translate(${e}) rotate(${210+n}) translate(${t+a})"
+  />
+
+
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+c}) translate(${t-a})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+f}) translate(${t-a})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+k}) translate(${t-a})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+o}) translate(${t})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+i}) translate(${t})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+$}) translate(${t})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+l}) translate(${t+a})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+s}) translate(${t+a})"
+  />
+
+  <circle fill="orange" stroke="black" r="5"
+          transform="rotate(-120) translate(${e}) rotate(${210+n}) translate(${t+a})"
+  />
+
+
+
+</svg>`,inputs:["html","co","r","ro","OI","OC","OO","CI","CC","CO","II","IC","IO"],outputs:[],output:void 0,assets:void 0,autodisplay:!0,autoview:!1,automutable:void 0});d({root:document.getElementById("cell-4"),expanded:[],variables:[]},{id:4,body:(r,e)=>{const t=r(e.range([0,180],{label:"y",value:0,step:10})),a=r(e.range([50,100],{label:"r",value:100,step:1})),l=r(e.range([5,20],{label:"ro",value:15,step:1})),s=r(e.range([10,90],{label:"co",value:60,step:1}));return{y:t,r:a,ro:l,co:s}},inputs:["view","Inputs"],outputs:["y","r","ro","co"],output:void 0,assets:void 0,autodisplay:!1,autoview:void 0,automutable:void 0});d({root:document.getElementById("cell-5"),expanded:[],variables:[]},{id:5,body:r=>({cd:r*Math.sqrt(3)}),inputs:["co"],outputs:["cd"],output:void 0,assets:void 0,autodisplay:!1,autoview:void 0,automutable:void 0});d({root:document.getElementById("cell-6"),expanded:[],variables:[]},{id:6,body:(r,e,t)=>{const a=180/Math.PI,l=({left:u,right:b,opposite:g})=>a*Math.acos((u*u+b*b-g*g)/(2*u*b)),s=l({left:r,right:e,opposite:r-t}),n=l({left:e,right:r-t,opposite:r}),o=l({left:r,right:e,opposite:r+t}),i=l({left:e,right:r+t,opposite:r}),$=l({left:e,right:r-t,opposite:r+t}),c=l({left:e,right:r+t,opposite:r-t}),f=l({left:r,right:e,opposite:r}),k=l({left:r-t,right:e,opposite:r-t}),m=l({left:r+t,right:e,opposite:r+t});return Object.assign(window,{foo:{CI:s,CC:f,CO:o,II:k,IC:n,IO:$,OI:c,OC:i,OO:m}}),{rad2deg:a,C:l,CI:s,IC:n,CO:o,OC:i,IO:$,OI:c,CC:f,II:k,OO:m}},inputs:["r","cd","ro"],outputs:["rad2deg","C","CI","IC","CO","OC","IO","OI","CC","II","OO"],output:void 0,assets:void 0,autodisplay:!1,autoview:void 0,automutable:void 0});d({root:document.getElementById("cell-7"),expanded:[],variables:[]},{id:7,body:(r,e,t,a,l,s,n,o)=>{r({CI:e,IC:t,CO:a,OC:l,CC:s,II:n,OO:o})},inputs:["view","CI","IC","CO","OC","CC","II","OO"],outputs:[],output:void 0,assets:void 0,autodisplay:!1,autoview:void 0,automutable:void 0});
